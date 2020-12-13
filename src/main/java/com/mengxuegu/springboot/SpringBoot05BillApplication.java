c8aa154bd0353b3e84e695efa8e259b297fb89d0
csz222333 <2747527180@qq.com>
2020-12-13 19:11:30 +0800

    代码验证成功

--- src/main/java/com/mengxuegu/springboot/SpringBoot05BillApplication.java
+++ src/main/java/com/mengxuegu/springboot/SpringBoot05BillApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("com.mengxuegu.springboot.mapper")
 @SpringBootApplication
+//修改修改
 public class SpringBoot05BillApplication {
 
     public static void main(String[] args) {
